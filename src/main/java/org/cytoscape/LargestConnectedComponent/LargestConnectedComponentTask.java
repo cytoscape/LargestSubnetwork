6ef9d73e48151e8bbb047a2289ad5c3ea2c46f17
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-15 23:50:08 -0800

    update

--- src/main/java/org/cytoscape/LargestConnectedComponent/LargestConnectedComponentTask.java
+++ src/main/java/org/cytoscape/LargestConnectedComponent/LargestConnectedComponentTask.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import java.util.Collections;
 import java.awt.EventQueue;
+import javax.swing.JFrame;
 
-import org.cytoscape.task.AbstractNetworkViewTask;
+
+import org.cytoscape.work.AbstractTask;
 import org.cytoscape.view.model.CyNetworkView;
 import org.cytoscape.work.TaskMonitor;
 import org.cytoscape.view.layout.PartitionUtil;
@@ -14,10 +16,10 @@ import org.cytoscape.view.layout.LayoutPartition;
 
 
 
-
-public class LargestConnectedComponentTask extends AbstractNetworkViewTask {
+public class LargestConnectedComponentTask extends AbstractTask {
 	protected List <LayoutPartition> partitionList = null;
 	protected double current_size = 0;
+	private CyNetworkView view;
 	protected ArrayList<Double> partlist = new ArrayList<>();
 	private void ShowMessage(String message) {
     EventQueue.invokeLater(new Runnable() {
@@ -28,15 +30,15 @@ public class LargestConnectedComponentTask extends AbstractNetworkViewTask {
     });
 }
 
-	LargestConnectedComponentTask(CyNetworkView v) {
-		super(v);
+	public LargestConnectedComponentTask(CyNetworkView view) {
+		this.view = view;
 	}
 
 	public void run(TaskMonitor tm) {
-		if(this.view == null){
+		if(view == null){
 			return;
 		}
-		partitionList = PartitionUtil.partition(this.view, false, null);
+		partitionList = PartitionUtil.partition(view, false, null);
 		for (LayoutPartition partition: partitionList) {
 				current_size = (double)partition.size();
 				partlist.add(current_size);
